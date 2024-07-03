3d0081f0b29b64f2dbe4a42eed111921a4650250
Vidhya0501 <vidhyasuri97@gmail.com>
2024-07-03 18:02:33 +0530

    e-commerce_be

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./database/db.js";
+import cors from "cors";
+
+// importing routes
+import userRoutes from "./routes/user.js";
+import productRoutes from "./routes/product.js";
+import cartRoutes from "./routes/cart.js";
+import addressRoutes from "./routes/address.js";
+import orderRoutes from "./routes/order.js";
+
+dotenv.config();
+const app = express();
+
+// using middlewares
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT;
+
+
+// using routes
+app.use("/api", userRoutes);
+app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", addressRoutes);
+app.use("/api", orderRoutes);
+
+app.use("/uploads", express.static("uploads")); //  this will help us to fetch image from server url
+
+app.listen(port, () => {
+  console.log(`App is running on the PORT:${port}`);
+  connectDb();
+});
